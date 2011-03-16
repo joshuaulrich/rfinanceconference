b201538e4b5bdafbbd9fb65918dc2faea291cd5f
Dirk Eddelbuettel <edd@debian.org>
2011-03-16 21:20:07 +0000

    making progress -- now with date headers

--- 2011/agenda/I_wish_I_had_php_skillz/writeAgenda.r
+++ 2011/agenda/I_wish_I_had_php_skillz/writeAgenda.r
@@ -10,17 +10,27 @@ for (i in 1:NROW(data)) {
     ## Example:
     ##<tr><td align="right">9:00am</td><td align="left"></td>- <td align="right">11:00am</td><td align="left"></td><td width="501"><b><font color="#894411">Ryan</font></b>: <font size="-1">Automated Trading with R and IBroker </font></td></tr>
 
+    if (row$type=="HEADER") {
+        cat('<tr><td colspan="5" align="left"><font size="+1">',
+            row$titleH,
+            '</font></td></tr>',
+            "\n", sep="", file=html)
+        next ## skip the rest
+    }
+
     ## start a row
-    cat('<tr><td align="right">', row$start,
-        '</td><td align="left"></td>- <td align="right">', row$end,
-        '</td><td align="left"></td><td width="501">',
+    cat('<tr><td align="right">', row$start,'</td><td align="left">',
+        ifelse(row$start!="     ",'-',' '),
+        '</td><td align="left">', row$end,
+        '</td><td align="left"></td><td width="501"><font size="-1">',
         #"\n\t",
         switch(row$type,
-               smallbreak =paste('<font size="-1" color="#595959">',    row$titleH, '</font>',           sep=""),
-               normalbreak=paste('<font size="0"  color="#595959"><b>', row$titleH, '</b></font>', sep=""),
-               keynote    =paste('<font size="0"  color="#00008C"><b>', row$authorH,'</b></font>: <font size="-1">', row$titleH, '</font>', sep=""),
-               talk       =paste('<font size="0"  color="#894411"><b>', row$authorH,'</b></font>: <font size="-1">', row$titleH, '</font>', sep=""),
-               lightning  =paste('<font size="0"  color="#CC7200"><b>', row$authorH,'</b></font>: <font size="-1">', row$titleH, '</font>', sep="")),
+               smallbreak =paste('<font color="#595959">',    row$titleH, '</font></font>',           sep=""),
+               normalbreak=paste('<font color="#595959"><b>', row$titleH, '</b></font></font>', sep=""),
+               tutorial   =paste('<font color="#595959"><b>', row$authorH,'</b></font>: ', row$titleH, '</font>', sep=""),
+               keynote    =paste('<font color="#00008C"><b>', row$authorH,'</b></font>: ', row$titleH, '</font>', sep=""),
+               talk       =paste('<font color="#894411"><b>', row$authorH,'</b></font>: ', row$titleH, '</font>', sep=""),
+               lightning  =paste('<font color="#CC7200"><b>', row$authorH,'</b></font>: ', row$titleH, '</font>', sep="")),
         '</td></tr>',
         "\n", sep="", file=html)
 }
